88b799f897c1c466c35ebb6db7178216fc61a3be
Joaquin Allue <Alluejoaquin@hotmail.com>
2025-04-28 12:41:10 -0300

    Le agregue la funcionalidad de likes a las publicaciones/posts

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\ComentarioController;
-use App\Http\Controllers\ImagenController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ComentarioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -45,6 +46,13 @@ Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
 Route::get('/{user:username}posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
+//rutas para agregar o eliminar comentarios
 Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
 
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+//rutas para agregar o eliminar likes
+Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
+
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
